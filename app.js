457be2883ddcfd8219b74bbbb0d4ef1bf143cc21
Alex Roman <Alex@romanink.net>
2015-06-16 00:51:16 -0700

    played with views a bit

--- app.js
+++ app.js
@@ -28,12 +28,12 @@ var Book = Backbone.Model.extend({
 });
 
 
-var book = new Book({
-	// name: "Testing a Model: A Novel",
-	// genre: "Occult Instructional",
-	// isbn: 684655,
-	_id: "5577df9dcc29878b499a6c0e"
-});
+// var book = new Book({
+// 	// name: "Testing a Model: A Novel",
+// 	// genre: "Occult Instructional",
+// 	// isbn: 684655,
+// 	_id: "5577df9dcc29878b499a6c0e"
+// });
 
 /* 
 	Create
@@ -88,3 +88,116 @@ var book = new Book({
 //         console.log("Something went wrong while deleting the model");
 //     }
 // });
+
+
+/*
+	Started working on collections
+ */
+
+var BooksCollection = Backbone.Collection.extend({
+	model: Book,
+	url: 'http://localhost:8081/api/books'
+});
+
+// var book1 = new Book({
+// 	name: "Testing a Model: A Novel",
+// 	genre: "Occult Instructional",
+// 	isbn: 685664,
+// });
+// var book2 = new Book({
+// 	name: "Learning About Collections",
+// 	genre: "Informative Fiction",
+// 	isbn: 684038,
+// });
+
+
+// var collection1 = new BooksCollection([book1, book2]);
+// var collection2 = new BooksCollection();
+// collection2.fetch();
+// book1.set('isbn', 666666);
+// console.log(book1.get('cid'));
+// var bookFromColl = collection1.at(0);
+// console.log(bookFromColl.get('name'));
+
+// for (var i = 0; i <= collection1.length -1; i++) {
+// 	console.log(collection1.at(i).get('name'));
+// };
+
+// for (var i = 0; i <= collection2.length -1; i++) {
+// 	console.log(collection2.at(i).get('name'));
+// };
+
+
+
+/*
+	Started working on views
+ */
+var book1 = new Book({
+	name: "Testing a Model: A Novel",
+	// genre: "Occult Instructional",
+	// isbn: 684655,
+	_id: "5577df9dcc29878b499a6c0e"
+});
+var bookView = Backbone.View.extend({
+	// initialize: function(){
+	// 	console.log('Sample view has been initialized');
+	// },
+	tagname: 'li',
+	model: Book,
+	render: function(){
+		this.$el.html('<li>' + this.model.get("name") + '</li>');
+		return this;
+	}
+});
+
+// var view1 = new bookView({model: book1});
+// var view1 = new bookView({el: $('#sampleDiv')});
+// var view1 = new bookView();
+
+var bookListView = Backbone.View.extend({
+	model: BooksCollection,
+
+	render: function(){
+		this.$el.html();
+		var self = this;
+		for(var i = 0; i < this.model.length; i++){
+			// create book view to render
+			var m_bookView = new bookView({model: this.model.at(i)});
+
+			// add book to list view
+			this.$el.append(m_bookView.$el);
+			m_bookView.render();
+		}
+		return this;
+	}
+});
+
+
+
+var book1 = new Book({ _id: 1, name: "Book 1" });
+var book2 = new Book({ _id: 2, name: "Book 2" });
+var book3 = new Book({ _id: 3, name: "Book 3" });
+var book4 = new Book({ _id: 4, name: "Book 4" });
+var book5 = new Book({ _id: 5, name: "Book 5" });
+var bookCollection = new BooksCollection([book1, book2, book3, book4, book5]);
+
+
+// var bookList = null;
+
+$(document).ready(function () {
+    bookList = new bookListView({ el: $("#bookList"), model: bookCollection });
+    bookList.render();
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
